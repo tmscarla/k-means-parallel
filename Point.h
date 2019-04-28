dd3077b2726023841dc3f12c39187b419031c837
russointroitoa <russointroitoa@gmail.com>
2019-04-28 18:53:12 +0200

    Adding Extract clusters

--- Point.h
+++ Point.h
@@ -11,10 +11,7 @@ using namespace std;
 
 struct Punto {
     double values[MAX_DIM];
-    int size;
-
-    /*TODO Teoricamente size non serve a niente; potremmo trasformarlo in un identificatore del punto, in modo
-     * da associare un valore del punto ad un cluster*/
+    int id;
 };
 
 #endif //KMEANSCLUSTERING_POINT_H
